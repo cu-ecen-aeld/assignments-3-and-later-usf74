15ac78b88f1aa9c2e04c7e9eaa01d12b71fd960f
usf74 <youssefbahgat@live.com>
2024-04-22 23:19:23 +0200

    meowwww :3

--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -2,6 +2,10 @@
 #include <stdbool.h>
 #include <stdarg.h>
 
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 bool do_system(const char *command);
 
 bool do_exec(int count, ...);
